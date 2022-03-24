0bef5076487be5f1158f60854b48ace8f2d7cda2
njsutliff <njsutliff@gmail.com>
2022-03-23 23:55:41 -0400

    DDL and DML now work actually, inserts and updates done, working on deletes

--- rcttc-data.sql
+++ rcttc-data.sql
@@ -1,62 +1,85 @@
-
 use tiny_theaters;
--- Theater    
+-- Theater insert
+select distinct theater, theater_address, theater_phone, theater_email from rcttc_data;
+
 insert into theater (
 theater_name,
 theater_address,
 theater_phone,
-theater_email)
-select distinct 
+theater_email
+)
+select distinct
 theater,
-theater_address, 
+theater_address,
 theater_phone,
 theater_email
 from rcttc_data;
-select * from theater;
--- Customer
-insert into customer(
-customer_email,
-customer_first,
-customer_last,
+
+select *
+from theater;
+
+-- Customer insert
+select distinct customer_first, customer_last, customer_email,customer_phone,customer_address from rcttc_data;
+
+insert into customer (
+customer_first, 
+customer_last, 
 customer_email,
 customer_phone,
-customer_address)
+customer_address
+)
 select distinct 
+customer_first, 
+customer_last, 
 customer_email,
-customer_last,
-customer_first,
 customer_phone,
 customer_address
 from rcttc_data;
-select * from customer;
 
--- play
-insert into play(
-play_name,
-play_date,
-theater
-)
+select * 
+from customer;
+
+
+-- Play insert
 select distinct
-play,
-play_date,
+`show`,
+`date`,
 theater
 from rcttc_data;
-select * from play;
--- ticket
-insert into ticket(
-ticket_price,
-seat,
+
+insert into play (
+play_name,
 play_date,
-customer_last
+theater_name
 )
 select distinct
-p.play,
-p.play_date,
-t.seat,
-t.ticket_price,
-c.customer_email
-from rcttc_data r
+`show`,
+`date`,
+theater
+from rcttc_data;
 
-inner join customer c on t.customer_email = c.customer_email  
-  inner join play p on t.play = p.play
-and p.play_date = t.play_date
+select *
+from play;
+
+-- Ticket insert
+/* P -> Play, T -> Ticket, C -> Customer R -> rcttc_data (file)
+*/
+select distinct p.play_date, p.play_name, t.seat, t.ticket_price, c.customer_email
+from rcttc_data r
+inner join play p on t.`show` = p.play_name
+and p.play_date = t.`date` -- 'show' here references play.play_name and 'date' = play.play_date
+inner join customer c on r.customer_email = c.customer_email;
+
+insert into ticket (
+play_date,
+play_name,
+seat,
+ticket_price,
+customer_email)
+select distinct p.play_date, p.play_name, t.seat, t.ticket_price, c.customer_email
+from rcttc_data t
+inner join play p on t.`show` = p.play_name
+and p.play_date = t.`date`
+inner join customer c on t.customer_email = c.customer_email;
+
+drop table if exists rcttc_data;
--- rcttc-schema.sql
+++ rcttc-schema.sql
@@ -3,41 +3,41 @@ create database tiny_theaters;
 use tiny_theaters;
 -- theater
 create table theater (
-theater_name  varchar(25) primary key not null,
-theater_address varchar(25) not null,
-theater_phone int(25) not null,
-theater_email varchar(25) not null
+	theater_name varchar(25) primary key,
+    theater_address varchar(100) not null,
+    theater_phone varchar(25) not null,
+    theater_email varchar(50) not null
 );
 
 -- customer
 
-create table customer(
-customer_last varchar(50) primary key not null,
-customer_first varchar(50)  not null,
-customer_email varchar(50) not null,
-customer_phone varchar(50) not null,
-customer_address varchar(50) not null
+create table customer (
+	customer_email varchar(50) primary key,
+    customer_first varchar(25) not null,
+    customer_last varchar(25) not null,
+    customer_phone varchar(25),
+    customer_address varchar(50)
+    );       
+-- play relates with customer with foreign key 'customer_email'
+
+create table play (
+	play_id int primary key auto_increment,
+	play_name varchar(100),
+    play_date date not null,
+    theater_name varchar(25) not null,
+    constraint fk_play_theater_name
+		foreign key (theater_name)
+		references theater(theater_name)
 );
-
--- play relates with theater with foreign key 'theater'
-
-create table play(
-play_id  int(25) primary key, -- auto inc
-play_name varchar(25)  not null,
-play_date varchar(25) not null,
-theater varchar(25) not null,
-constraint fk_play_theater_name
-	foreign key (theater_name)
-    references theater(theater_name)
+-- ticket
+create table ticket (
+	ticket_id int primary key auto_increment,
+    ticket_price decimal(4,2) not null,
+    seat varchar(2) not null,
+    play_date date not null,
+    play_name varchar(100) not null,
+    customer_email varchar(50) not null,
+    constraint fk_ticket_customer_email
+		foreign key (customer_email)
+		references customer(customer_email)
 );
-
-create table ticket(
-ticket_id int(50) primary key, -- auto inc
-ticket_price decimal(5) not null,
-seat varchar(50) not null,
-play_date varchar(25) not null,
-customer_last varchar(50) not null,
-constraint fk_ticket_customer_last
-	foreign key (customer_last)
-    references customer(customer_last)
-) ;
